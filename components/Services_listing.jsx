54d652f24d1465e918ef1682030a85d80e74ab9f
MBJ-97 <mehdiibeldjoudi@gmail.com>
2022-12-17 12:25:17 +0100

    contact and about  sections content

--- components/Services_listing.jsx
+++ components/Services_listing.jsx
@@ -5,7 +5,7 @@ import Arrow from "../public/assets/arrow.png";
 export default function Services_listing({ name, description, image }) {
   return (
     <Link href="#contact">
-      <div className="service-box w-full p-8 rounded-lg md:w-3/4 hover:cursor-pointer border-2 border-black hover:border-white transition ease-in delay-50">
+      <div className="service-box w-full p-8 rounded-lg md:w-3/4 hover:cursor-pointer border-2 border-black hover:border-orange transition ease-in delay-50">
         <div className="icon flex items-center justify-between mb-6">
           <Image src={image} width={46} height={46} />
           <div className="arrow opacity-0">
