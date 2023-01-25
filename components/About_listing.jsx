345a4425730e0f18c7d9aa289bc1a20a7a37248d
MBJ-97 <mehdiibeldjoudi@gmail.com>
2023-01-25 22:30:50 +0100

    Fixed design in hero and about cards

--- components/About_listing.jsx
+++ components/About_listing.jsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function About_listing({ image_link, title, description }) {
   return (
-    <div className="w-full md:w-1/3 py-12 px-6 md:px-10 rounded-lg shadow-custom hover:-translate-y-2 transition ease-in delay-100">
+    <div className="w-full md:w-1/3 py-12 px-6 md:px-10 rounded-lg  hover:shadow-custom hover:-translate-y-2 transition ease-in delay-100">
       <div className="mb-8 flex justify-start">
-        <Image src={image_link} alt={title} width={40} height={40} />
+        <Image src={image_link} alt={title} width={50} height={50} />
       </div>
-      <h3 className="font-bold text-orange">{title}</h3>
+      <h3 className="font-bold text-orange text-lg mb-2">{title}</h3>
       <p>{description}</p>
     </div>
   );
--- components/Hero_section.jsx
+++ components/Hero_section.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const Hero_section = () => {
   return (
     <>
-      <div className="pt-20 pb-8 md:pt-28 bg-black text-white flex flex-wrap md:flex-nowrap justify-between h-screen px-4 md:px-20">
-        <div className="text max-w-lg pt-0 sm:pt-8 ">
+      <div className="border-white pt-20 pb-8 md:pt-28 bg-black text-white flex flex-wrap justify-center md:flex-nowrap gap-6 h-screen px-4 md:px-20">
+        <div className="text-section max-w-lg pt-0 sm:pt-8">
           <h1 className="mb-6 text-3xl sm:text-5xl font-semibold text-orange">
             Votre partenaire logistique fiable.
           </h1>
@@ -15,7 +15,7 @@ const Hero_section = () => {
             nous laisser le reste !
           </h2>
         </div>
-        <div className="image">
+        <div className="image-section">
           <Image
             src={Hero}
             width={612}
