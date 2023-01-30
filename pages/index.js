f3016166ca82087bf9d6860cc169ce4067606687
MBJ-97 <mehdiibeldjoudi@gmail.com>
2023-01-30 20:42:51 +0100

    Changed SEO title tags name

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,14 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Vectorz | Votre partenaire logistique</title>
+        <title>Vectorz</title>
         <meta
           name="description"
           content="Vectorz vous offre son service de livraison en Algérie qui couvre tout vos besoins (E-commerce, Colis et courrier express, retrait et dépôt de cahier de charges & services de déménagement et de manutention... ) logistiques dans les 58 wilayas"
         />
         <meta
           name="keywords"
-          content="Vectorz, livraison e-commerce, logistique, Algérie, e-com"
+          content="Vectorz, livraison e-commerce, logistique, Algérie, e-commerce"
         />
         <meta
           name="google-site-verification"
@@ -26,10 +26,7 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
         {/* OG tags  */}
-        <meta
-          property="og:title"
-          content="Vectorz | Services de logistique en Algérie"
-        />
+        <meta property="og:title" content="Vectorz" />
         <meta
           property="og:description"
           content="Service de livraison en Algérie qui couvre tout vos besoins (E-commerce, Colis et courrier express, retrait et dépôt de cahier de charges... ) logistiques dans les 58 wilayas"
